22ae687328a0e6101a3682906b1735dc53190f04
stasila89 <73433886+stasila89@users.noreply.github.com>
2020-11-09 16:30:20 +0300

    Update test.php

--- 09.11.2020/function/test.php
+++ 09.11.2020/function/test.php
@@ -1,29 +1,25 @@
 <?php
 //ДЗ на 04.11.2020 Создать файл обработчик и вывести результат выбранной функции
-include_once './testFunction.php';
-if (!empty($_POST)){
-    $text =$_POST['text1'];
+include_once 'testFunction.php';
+if (!empty($_POST)) {
+    $text = $_POST['text1'];
     $nameOfFunction = $_POST['nameOfFunction'];
 
-    if ($nameOfFunction == 'funcSpaces'){
-echo "<h2> Текст без пробелов : </h2>. <br>";
-return funcSpaces($text);
-    }
-    elseif ($nameOfFunction == 'shortWords'){
-        echo "<h2> Слова с количеством от 3 до 5 букв : </h2>. <br>";
-         return shortWords($text);
-    }
-    elseif ($nameOfFunction == 'palindrome'){
-        echo "<h2> Является ли палиндромом : </h2>. <br>";
-        return palindrome($text);
-    }
-    elseif ($nameOfFunction == 'longestWord'){
-        echo "<h2> Самое длинное слово : </h2>. <br>";
-        return longestWord($text);
-    }
-    elseif ($nameOfFunction == 'capitalLetter'){
-        echo "<h2> Количество прописных и строчных букв : </h2>. <br>";
-        return capitalLetter($text);
+    if ($nameOfFunction == 'funcSpaces') {
+        echo "<h4> Текст без пробелов : </h4> <br>";
+        echo funcSpaces($text);
+    } elseif ($nameOfFunction == 'shortWords') {
+        echo "<h4> Слова с количеством от 3 до 5 букв : </h4> <br>";
+        echo shortWords($text);
+    } elseif ($nameOfFunction == 'palindrome') {
+        echo "<h4> Является ли палиндромом : </h4> <br>";
+        echo palindrome($text);
+    } elseif ($nameOfFunction == 'longestWord') {
+        echo "<h4> Самое длинное слово : </h4> <br>";
+        echo longestWord($text);
+    } elseif ($nameOfFunction == 'capitalLetter') {
+        echo "<h4> Количество прописных и строчных букв : </h4> <br>";
+        echo capitalLetter($text);
     }
 
 }
