93af349b9974bba5c51ced6697c966f0664d67dc
stasila89 <73433886+stasila89@users.noreply.github.com>
2020-11-30 14:39:07 +0300

    Update save_page.php

--- 30.11.2020/admin/template/types/save_page.php
+++ 30.11.2020/admin/template/types/save_page.php
@@ -1,6 +1,11 @@
 <div id="page-wrapper">
     <div class="row">
-        <?php
+         <?php
+        $title = $_POST['title'];
+        $imgUrl = $_FILES['page_img']['name'];
+        $content = $_POST['content'];
+        $author = $_POST['author'];
+        $category = $_POST['category'];
         if (!empty($_POST)) {
 
             if ($_FILES['page_img']['size'] > 0) {
@@ -10,10 +15,11 @@
             }
         }
 
-        $sql = "INSERT INTO `pages` (`title`, `img`, `content`, `author`, `category`)
-    VALUES ('{$_POST['title']}', '{$imgUrl}', '{$_POST['content']}', '{$_POST['author']}', '{$_POST['category']}')
-";
-   mysqli_query($connection, $sql);
+        $sql = mysqli_prepare($connection, 'INSERT INTO pages (`title`, `img`, `content`, `author`, `category`)
+    VALUES (?,?,?,?,?)');
+        mysqli_stmt_bind_param($sql, "sssss", $title, $imgUrl, $content, $author, $category);
+        mysqli_stmt_execute($sql);
+
 
         ?>
      
