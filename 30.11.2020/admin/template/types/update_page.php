38e7822e3b2f2142cc6ef2781de03ecd36d5ca6e
stasila89 <73433886+stasila89@users.noreply.github.com>
2020-11-30 14:40:08 +0300

    Update update_page.php

--- 30.11.2020/admin/template/types/update_page.php
+++ 30.11.2020/admin/template/types/update_page.php
@@ -1,17 +1,22 @@
 <?php
+$ids = $_GET['id'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+$author = $_POST['author'];
+$category = $_POST['category'];
 
-$sql = "UPDATE pages SET ";
-foreach ($_POST as $key => $value){
-
-    $sql .= "{$key} = '$value',";
+if (isset($_FILES) && $_FILES['page_img']['size'] > 0){
+    $imgUrl = 'downloaded_files/' . $_FILES['page_img']['name'];
+    $stmt = mysqli_prepare($connection, "UPDATE pages SET title=?, img=?, content=?, author=?, category=? WHERE id=?");
+    mysqli_stmt_bind_param($stmt, "sssssd", $title, $imgUrl, $content, $author, $category, $ids);
+    mysqli_stmt_execute($stmt);
+}else {
+    $stmt = mysqli_prepare($connection, "UPDATE pages SET title=?, content=?, author=?, category=? WHERE id=?");
+    mysqli_stmt_bind_param($stmt, "ssssd", $title,  $content, $author, $category, $ids);
+    mysqli_stmt_execute($stmt);
 }
-$sql = substr($sql,0, -1);
-$sql .= 'where id ='.$_GET['id'];
-//echo $sql;
-mysqli_query($connection, $sql);
 
 ?>
-
 <div id="page-wrapper">
     <div class="row">
         <h2> Корректировки внесены</h2>
